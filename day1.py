985a02fae8b79e353583cbd74eba922a3099d8c2
soohyun <soohyun@neurophet.com>
2023-11-23 18:26:02 +0900

    day 1!

--- day1.py
+++ day1.py
@@ -1,1 +1,15 @@
-print("hello")
+import aocd
+
+input = aocd.get_data(day=1, year=2022).split('\n')
+
+calorieslist = []
+tmp = 0
+for calories in input:
+    if calories != "": tmp+=int(calories)
+    else:
+        calorieslist.append(tmp)
+        tmp = 0
+
+calorieslist = sorted(calorieslist, reverse=True)
+print("Part One: ", calorieslist[0])
+print("Part Two: ", sum(calorieslist[:3]))
